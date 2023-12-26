2b94b0d03bc02265e2c73855e24b4cdb09be907a
Name-3R1C <ericfeng94@hotmail.com>
2023-12-26 02:22:58 -0800

    add schema and seeds

---
+++ server/seeds/01_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES 
+('Admin', 'User', 'admin@mail.com', '123'), 
+('Eva', 'Stanley', 'sebastianguerra@ymail.com', '123'), 
+('Louisa', ' Meyer', 'jacksonrose@hotmail.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.'), 
+('Dominic', ' Parks', 'victoriablackwell@outlook.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.');
