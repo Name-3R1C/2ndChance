2d492534fb8aa416799b302bee2c9e125afa895f
Name-3R1C <ericfeng94@hotmail.com>
2024-01-08 21:41:00 -0800

    creating item component

--- server/routes/items.js
+++ server/routes/items.js
@@ -3,10 +3,10 @@ const db = require("../db");
 
 router.get("/items", async(req, res) => {
   try {
-    const properties = await db.getAllItems();
-    res.send({ properties });
+    const items = await db.getAllItems();
+    res.send({ items });
   } catch (error) {
-    console.error("Error fetching all properties:", error);
+    console.error("Error fetching all items:", error);
     res.status(500).send("Internal Server Error");
   }
 });
